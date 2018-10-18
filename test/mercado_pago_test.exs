66419393daca90143b96972c989c7a75f72af12e
José Esteves <ajesteves@gmail.com>
2018-10-18 13:50:06 -0300

    add basic unit test

--- test/mercado_pago_test.exs
+++ test/mercado_pago_test.exs
@@ -2,7 +2,9 @@ defmodule MercadoPagoTest do
   use ExUnit.Case
   doctest MercadoPago
 
-  test "greets the world" do
-    assert MercadoPago.hello() == :world
+  test "Gets Valid Payment Link" do
+    MercadoPago.get_payment_link("test title", "test description", 150)
+    |> String.match?(~r/mercadopago.*\/checkout\/start\?pref_id/)
+    |> assert
   end
 end
