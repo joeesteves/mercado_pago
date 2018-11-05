c4d2e7c162afa90380b4a126aced1321bcf0723e
José Esteves <ajesteves@gmail.com>
2018-11-05 10:05:33 -0300

    Update patch version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MercadoPago.MixProject do
   def project do
     [
       app: :mercado_pago,
-      version: "0.2.0",
+      version: "0.2.2",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps()
